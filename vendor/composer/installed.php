d310dec3a9b94b8645b9d2bb8ba950503adc9c86
Shakhawat Hossain <shakhawat579@gmail.com>
2026-05-02 21:07:16 +0600

    upload vendor files also to minimize hassle for tester

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'shakhawat/team',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd25419709fb9c366420ae935feb76eeb34e2b736',
+        'type' => 'plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'shakhawat/team' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd25419709fb9c366420ae935feb76eeb34e2b736',
+            'type' => 'plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
